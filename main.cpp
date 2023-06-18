82b14e31a8346dc31cce5ffd5cbfd1e918df2c6f
miraclehhy <1197468418@qq.com>
2023-06-18 15:55:58 +0800

    v1.2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <stdio.h>
-#include <iostream>
 #include <winsock2.h>
+#pragma comment(lib,"ws2_32.lib")
 
+int iferror(int rec_data,int error_data,char* showinfo)
+{
+    if(rec_data == error_data)
+    {
+        //perror(showinfo);
+        getchar();
+        return -1;
+    }
+    return 0;
+}
 
 int main()
 {
-    std::cout << "hello!" << std::endl;
+    printf("欢迎使用该服务器！\n");
+    WSADATA wsdata;
+    int isok = WSAStartup(MAKEWORD(2,2),&wsdata);
+    iferror(isok,WSAEINVAL,"申请socket错误");
+    SOCKET server = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);//ipv4,tcp协议
+    iferror(server,INVALID_SOCKET,"创建socket失败");
 
+
+    getchar();
     return 0;
 }
