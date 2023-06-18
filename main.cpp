10ae93a68a6458ba892075efeab0791a4ec9f7f3
miraclehhy <1197468418@qq.com>
2023-06-18 16:08:35 +0800

    v1.3

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include <winsock2.h>
 #pragma comment(lib,"ws2_32.lib")
 
-int iferror(int rec_data,int error_data,char* showinfo)
+int iferror(int rec_data,long long int error_data,char* showinfo)
 {
     if(rec_data == error_data)
     {
-        //perror(showinfo);
+        perror(showinfo);
         getchar();
         return -1;
     }
@@ -15,12 +15,14 @@ int iferror(int rec_data,int error_data,char* showinfo)
 
 int main()
 {
-    printf("欢迎使用该服务器！\n");
+    printf("欢迎使用该服务器！");
     WSADATA wsdata;
     int isok = WSAStartup(MAKEWORD(2,2),&wsdata);
-    iferror(isok,WSAEINVAL,"申请socket错误");
+    char* wrong = (char*)"错误";
+    iferror(isok,WSAEINVAL,wrong);
     SOCKET server = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);//ipv4,tcp协议
-    iferror(server,INVALID_SOCKET,"创建socket失败");
+    wrong = (char*)"创建socket失败";
+    iferror(server,INVALID_SOCKET,wrong);
 
 
     getchar();
